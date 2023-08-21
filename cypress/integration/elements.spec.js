9daa6806cc8bab00219bf83ae1ef9daf9683ee7e
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-08-21 17:08:04 -0300

    studying the use of texts and links within tests

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    it('Texts', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+        cy.get('body').should('contain', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') // Have.text quer o texto exato
+    })
+    
+    it('Links', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+        
+        cy.reload() // recarrega a tela
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
+})

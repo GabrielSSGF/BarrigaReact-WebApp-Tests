848555cbbb940dae8157f72c6c662851d2b72f66
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-08-22 15:00:38 -0300

    implementation of Text Fields and Radio Button tests

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -25,4 +25,38 @@ describe('Work with basic elements', () => {
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
+
+    it('TextField', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+        
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+        
+            cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}') // backspace deletes de last itens on a string
+            .should('have.value', 'Teste123')
+        
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay:100})
+            .should('have.value', 'acerto')
+    })
+
+    it('RadioButton', () => {
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
+        
+        cy.get('#formSexoMasc').should('not.be.checked')
+    
+        cy.get("[name='formSexo']")
+            .should('have.length', 2)
+    })
+
+
 })

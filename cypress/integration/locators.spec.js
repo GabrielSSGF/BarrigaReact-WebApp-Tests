dae5531764879f66b831fe5f679086770ff39bfe
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-08-26 22:36:50 -0300

    implementing different forms of locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -9,7 +9,12 @@ describe('Work with basic elements', () => {
         cy.reload()
     })
 
-    it('...', () => {
-
+    it('Jquery Selector', () => {
+        cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
+        // cy.get('table#tabelaUsuarios tbody > tr td:nth-child(3) > input').click() // Da errado pois ele retorna 5 elementoss
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input').click() // definindo a posição correta do tr, ele pega o primeiro elemento
+        cy.get("[onclick*='Francisco']")
+        cy.get("#tabelaUsuarios td:contains('Doutorado'):eq(0) ~ td:eq(3) > input")
+        cy.get("#tabelaUsuarios tr:contains('Doutorado'):eq(0) td:eq(6) input")
     })
 })

dbdd1e866dee57deec0ca920f386a66157972529
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-08-27 00:31:58 -0300

    implemented dynamic testing

---
+++ cypress/integration/dynamic.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Alerts', () => {
+    before(() => { // Executa antes de todos os testes
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    const foods = ['Carne', 'Frango', 'Pizza', 'Vegetariana']
+    foods.forEach(food => {
+        it(`register with ${food}`, () => {
+            cy.get('#formNome').type('Usuario')
+            cy.get('#formSobrenome').type('Qualquer')
+            cy.get(`[name=formSexo][value=F]`).click()
+            cy.get(`#formComida${food}`).click()
+            cy.get('#formEscolaridade').select('Doutorado')
+            cy.get('#formEsportes').select('Corrida')
+            cy.get('#formCadastrar').click()
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        })
+    })
+})
+    

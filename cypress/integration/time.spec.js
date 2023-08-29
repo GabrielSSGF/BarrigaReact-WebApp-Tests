cf42eebc2ae3b76325b84b7bc10775acb3e167f2
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-08-29 17:03:07 -0300

    using the tick function to move through time

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -20,4 +20,27 @@ describe('Alerts', () => {
         cy.get('#resultado > span').should('contain', '10/04/2012')
         
     })
+
+    it.only('Changes time to the future', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '1693')
+        cy.get('#resultado > span').invoke('text').should('be.greaterThan', 1693338968739)
+        
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('be.at.most', 0)
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').should('be.lessThan', 1000)
+        
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('be.at.least', 5000)
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('be.at.least', 15000)
+        
+        
+
+    })
 })

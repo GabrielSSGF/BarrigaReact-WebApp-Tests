1df5fc5905e50cbdb3ce629fa9a681e26219db3a
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-09-02 23:11:12 -0300

    Added tests for account update

--- cypress/integration/applicationTests/functional.spec.js
+++ cypress/integration/applicationTests/functional.spec.js
@@ -3,6 +3,12 @@
 describe('Functional tests', () => {
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
+
+        // Login
+
+        cy.get('.input-group > .form-control').type('accountEmail@mail.com')
+        cy.get(':nth-child(2) > .form-control').type('accountPassword123')
+        cy.get('.btn').click()
     })
 
     // it('Account creation', () => {
@@ -15,14 +21,14 @@ describe('Functional tests', () => {
     //     cy.get('.btn').click()
     // })
 
-    it('Login', () => {
-        // cy.get('.input-group > .form-control').type('gabrielTest@mail.com')
-        cy.get('.input-group > .form-control').type('accountEmail@mail.com')
-        cy.get(':nth-child(2) > .form-control').type('accountPassword123')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('exist')
-        cy.get('.toast-message').should('contain', 'Bem vindo')
-    })
+    // it('Login', () => {
+    //     // cy.get('.input-group > .form-control').type('gabrielTest@mail.com')
+    //     cy.get('.input-group > .form-control').type('accountEmail@mail.com')
+    //     cy.get(':nth-child(2) > .form-control').type('accountPassword123')
+    //     cy.get('.btn').click()
+    //     cy.get('.toast-message').should('exist')
+    //     cy.get('.toast-message').should('contain', 'Bem vindo')
+    // })
 
     it('Creating other accounts', () => {
         cy.get('.dropdown-toggle').click()
@@ -31,4 +37,24 @@ describe('Functional tests', () => {
         cy.get('.btn').click()
         cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
     })
+
+    it('Should update an account', () => {
+        cy.get('tbody')
+            .contains('Account test')
+            .parent('tr')
+            .invoke('index')
+            .then((index) => {
+                const accountIndex = index + 1;
+                const buttonSelector = `:nth-child(${accountIndex}) > :nth-child(2) > :nth-child(1) > .far`
+                
+                cy.get(buttonSelector).click()
+                cy.get('.form-control')
+                    .clear()
+                    .type('Account modified')
+                cy.get('.btn').click()
+            });
+    
+        cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso');
+    });
 })
+

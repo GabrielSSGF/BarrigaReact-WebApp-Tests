1a4b2d2c4011eb155897d1da5243b81d9b4e4075
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-09-19 15:26:55 -0300

    Added tests for balance gets and transaction deletion

--- cypress/integration/applicationTests/backend.spec.js
+++ cypress/integration/applicationTests/backend.spec.js
@@ -99,4 +99,70 @@ describe('Should test at a functional level', () => {
         cy.get('@response').its('status').should('be.equal', 201)
         cy.get('@response').its('body.id').should('exist')
     })
+
+    it('Should get balance', () => {
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}`}
+        }).then(response => {
+            let saldoConta = null
+            response.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('534.00')
+        })
+
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            headers: { Authorization: `JWT ${token}`},
+            qs: { descricao: 'Movimentacao 1, calculo saldo'}
+        }).then(response => {
+            console.log(response.body[0])
+            cy.request({
+                url: `/transacoes/${response.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}`},
+                body: {
+                    status: true,
+                    data_transacao: Cypress.moment(response.body[0].data_transacao).format('DD/MM/YYYY'),
+                    data_pagamento: Cypress.moment(response.body[0].data_Pagamento).format('DD/MM/YYYY'),
+                    descricao: response.body[0].descricao,
+                    envolvido: response.body[0].envolvido,
+                    valor: response.body[0].valor,
+                    conta_id: response.body[0].conta_id,
+                }
+            }).its('status').should('be.equal', 200)
+        })
+
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}`}
+        }).then(response => {
+            let saldoConta = null
+            response.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('4034.00')
+        })
+        
+    })
+
+    it('Should remove a transaction', () => {
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            headers: { Authorization: `JWT ${token}`},
+            qs: { descricao: 'Movimentacao para exclusao'}
+        }).then(response => {
+            cy.request({
+                url: `/transacoes/${response.body[0].id}`,
+                method: 'DELETE',
+                headers: { Authorization: `JWT ${token}`},
+            }).its('status').should('be.equal', 204)
+        })
+
+    })
 })
